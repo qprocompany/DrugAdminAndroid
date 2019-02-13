47b29e3b0b7142dea2d89f3a9607185ae86193b1
andryrdna <sim.andry1@gmail.com>
2019-02-13 10:10:44 +0700

    Maintenanceinfo, Detail Bed Cleaning

--- app/src/main/java/edmt/dev/androidcamera2api/DetailBedcleaning.java
+++ app/src/main/java/edmt/dev/androidcamera2api/DetailBedcleaning.java
@@ -1,13 +1,17 @@
 package edmt.dev.androidcamera2api;
 
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class DetailBedcleaning extends AppCompatActivity {
     TextView bedid1,tgl,start,stop;
+    Button finish;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,30 +22,39 @@ public class DetailBedcleaning extends AppCompatActivity {
         tgl = findViewById(R.id.textView9);
         start = findViewById(R.id.textView13);
         stop = findViewById(R.id.textView11);
-        new Detail(Bedcleaning.bedid).execute();
+        new Detail().execute();
+
+        finish = findViewById(R.id.button);
+        finish.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                OpenMainActivity();
+            }
+        });
     }
 
     class Detail extends AsyncTask<String, String, String>
     {
-        private String bedid;
-        public Detail(String bedid) {
-            this.bedid = bedid;
-        }
-        @Override
         protected String doInBackground(String... strings) {
             CallSoap cs = new CallSoap();
-            String data = cs.DetailCleaning(Login.username1 ,bedid);
+            String data = cs.DetailCleaning(Login.username1 ,Bedcleaning.bedid);
             return data;
         }
         @Override
         protected void onPostExecute(String s) {
             super.onPostExecute(s);
-            String data[] = s.split("_");
+            Toast.makeText(DetailBedcleaning.this,s,Toast.LENGTH_SHORT).show();
             bedid1.setText(Bedcleaning.bedid);
+            String data[] = s.split("_");
             String temp[] = data[1].split(" ");
-            tgl.setText(data[1].substring(0,data[1].indexOf(" ")));
+            tgl.setText(data[1].substring(1,data[1].indexOf(" ")));
             start.setText(data[1].substring(data[1].indexOf(" ")));
             stop.setText(data[2].substring(data[1].indexOf(" ")));
         }
+    }
+
+    public void OpenMainActivity(){
+        Intent intent = new Intent(this, Menu.class);
+        startActivity(intent);
     }
 }
